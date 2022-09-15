283a12f86896a4511ee1c86949e3db3ac533fdec
AsmaTycoon <cristiansalzgeber@hotmail.com>
2022-09-15 03:59:07 -0300

    proyecto finalizado

--- src/app/components/proyecto/edit-proyectos.component.ts
+++ src/app/components/proyecto/edit-proyectos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Proyectos } from 'src/app/model/proyectos';
+import { ProyectosService } from 'src/app/service/proyectos.service';
 
 @Component({
   selector: 'app-edit-proyectos',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-proyectos.component.css']
 })
 export class EditProyectosComponent implements OnInit {
+  proyecto: Proyectos = null;
 
-  constructor() { }
-
+  constructor(private proyectoS: ProyectosService, private activatedRoute: ActivatedRoute, private router: Router) { }
+  
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.proyectoS.detail(id).subscribe(
+      data => {
+        this.proyecto = data;
+      }, err => {
+        alert ("Error al modificar");
+        this.router.navigate(['']);
+      }
+    )
   }
 
+  onUpdate(){
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.proyectoS.update(id, this.proyecto).subscribe(
+      data => {
+        this.router.navigate(['']);
+      }, err => {
+        alert("Error al modificar el proyecto");
+        this.router.navigate(['']);
+      }
+    )
+  }
 }
